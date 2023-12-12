cc882e828c68de5fc544fa3bba81d1907671deaa
anggiansyah <anggiansyahslv8@gmail.com>
2023-12-12 10:59:41 +0700

    UTS

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/userdata.dart';
+import 'package:flutter_app/useritem.dart';
+import 'package:flutter_app/userlist.dart';
+
+void main() {
+  // UserData userUmam = UserData(umam, 22, miftahulumam862@gmail.com);
+  runApp(UserList());
+}
